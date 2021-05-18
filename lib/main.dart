de9a6077b05a1dfebcdb04b6bedb660636ca851e
rasheed <hazelrasheed@gmail.com>
2021-05-18 12:58:31 +0300

    start new network project

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,22 @@
 import 'package:flutter/material.dart';
 
-import 'package:http/http.dart' as http;
-
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'EarthQuake App',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        scaffoldBackgroundColor: Colors.white,
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Material App Bar'),
+        ),
+        body: Center(
+          child: Container(
+            child: Text('Hello World'),
+          ),
+        ),
       ),
-      home: HomeScreen(),
     );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: SafeArea(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [],
-      ),
-    ));
   }
 }
