6b5128cee2a0b57f06ad99ea59ca6d8ae4c09611
rasheed <hazelrasheed@gmail.com>
2021-05-18 13:32:04 +0300

    created listView.builder and given data from map

--- lib/main.dart
+++ lib/main.dart
@@ -31,16 +31,24 @@ class ListViewFromList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final List<String> entries = <String>['A', 'B', 'C'];
-    final List<int> colorCodes = <int>[600, 500, 100];
-
+    final List<int> colorCodes = <int>[600, 500, 100, 100];
+    final Map<String, String> users = {
+      'name': 'rashee',
+      'surname': 'muharram',
+      'countery': 'yemen',
+      'job': 'developer',
+    };
+    // get key lis and users["key item"],
+    List values = users.values.toList();
+    List keys = users.keys.toList();
     return ListView.builder(
         padding: const EdgeInsets.all(8),
-        itemCount: entries.length,
+        itemCount: values.length,
         itemBuilder: (BuildContext context, int index) {
           return Container(
             height: 50,
             color: Colors.amber[colorCodes[index]],
-            child: Center(child: Text('Entry ${entries[index]}')),
+            child: Center(child: Text('${keys[index].toString().toUpperCase()}: ${values[index]}')),
           );
         });
   }
