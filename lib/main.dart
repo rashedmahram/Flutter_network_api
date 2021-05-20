ec79c9545c16bae1eed844e8334218baf5e79aad
rasheed <hazelrasheed@gmail.com>
2021-05-20 22:59:00 +0300

    Hive

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,15 @@ import 'package:earth_quake_app/screens/shared_preferences/all_about_sharedprefe
 import 'package:earth_quake_app/screens/shared_preferences/home_screen.dart';
 import 'package:earth_quake_app/shared_prefrences/shared_view.dart';
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
 
 void main() async {
+  // get the path for app
+  final appDocumentDirectory = await path_provider.getApplicationDocumentsDirectory();
+  // initilaize the hive
+  Hive.init(appDocumentDirectory.path);
+
   runApp(MyApp());
 }
 
@@ -22,14 +29,26 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Netwrok App',
-      initialRoute: '/hive',
-      routes: {
-        '/shared': (context) => SharedView(),
-        '/city': (context) => CityScreen(),
-        '/sharedPref': (context) => SharedDataScreen(),
-        '/sharedPre': (context) => SharedPref(),
-        '/hive': (context) => HiveQuestions(),
-      },
+      // initialRoute: '/hive',
+      // routes: {
+      //   '/shared': (context) => SharedView(),
+      //   '/city': (context) => CityScreen(),
+      //   '/sharedPref': (context) => SharedDataScreen(),
+      //   '/sharedPre': (context) => SharedPref(),
+      //   '/hive': (context) => HiveQuestions(),
+      // },
+      home: FutureBuilder(
+        future: Hive.openBox("name"),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasError) {
+              return Text(snapshot.error);
+            } else {
+              return HiveQuestions();
+            }
+          }
+        },
+      ),
     );
   }
 }
